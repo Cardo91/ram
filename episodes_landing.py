86d381bb9b8cf8792defec08f1e71329cefc161f
Cardo91 <131213003+Cardo91@users.noreply.github.com>
2023-04-18 21:26:18 -0300

    Update episodes_landing.py

--- episodes_landing.py
+++ episodes_landing.py
@@ -2,10 +2,8 @@ import sqlite3
 import json
 from graphqlclient import GraphQLClient
 
-# Crea un cliente de GraphQL y establece la URL de la API de Rick and Morty
 client = GraphQLClient('https://rickandmortyapi.com/graphql')
 
-# Define la consulta GraphQL para obtener todos los episodios
 query ='''
     query getAllEpisodes($page: Int!) {
         episodes(page: $page) {
@@ -22,30 +20,19 @@ query ='''
     }
 '''
 
-# Define una función para obtener todos los episodios de la API utilizando la paginación
 def get_all_episodes():
-    # Inicializa una lista vacía para almacenar los episodios
     all_episodes = []
-    # Establece el número de página inicial
     page = 1
-    # Bucle para obtener todos los episodios de la API utilizando la paginación
     while True:
-        # Envía la consulta GraphQL con el número de página actual
         result = client.execute(query, {'page': page})
-        # Analiza la respuesta JSON en un diccionario de Python
         response = json.loads(result)
-        # Agrega los episodios de la página actual a la lista
         all_episodes += response['data']['episodes']['results']
-        # Verifica si hay más páginas para obtener
         next_page = response['data']['episodes']['info']['next']
         if next_page is None:
             break
-        # Incrementa el número de página
         page += 1
-    # Devuelve la lista de todos los episodios
     return all_episodes
 
-# Llama a la función para obtener todos los episodios de la API
 all_episodes = get_all_episodes()
 
 conn = sqlite3.connect('C:\db/rick_and_morty.db')
